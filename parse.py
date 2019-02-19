c20f3048435c767c6bfc0a7ca5977b926d63ab55
linux box <linux@ishans-mbp.wireless.rit.edu>
2019-02-18 22:49:46 -0500

    added support for file input using command line

--- parse.py
+++ parse.py
@@ -1,19 +1,33 @@
+"""
+filename: parse.py
+author: ishanguliani
+website: github.com/ishanguliani/json-parser-beautify
+description:
+
+view a cleaner version of your json file. Helpful for viewing a part of a huge dataset
+before importing it into your DATABASE
+"""
+
 import json
+import sys
 
 def main():
-	i = 0
-	with open("business.json") as f:
-		#for line in f.readline():
-		print('inside open')
-		line = f.readline()
-		jsonloaded = json.loads(line)
-		
-		#	if i > 4:
-		#		break;
-		print(json.dumps(jsonloaded, indent=2))
-		#break
-		#i += 1
-	pass
+    count = 0
+    try:
+            filename = str(sys.argv[1])
+            with open(filename) as f:
+                print(json.dumps(json.loads(f.readline().strip()), indent=2))
+                count += 1
+                response = str(input('The first line is printed, do you want to see the whole file ?')).lower()
+                if response.__contains__('n'):
+                    sys.exit(0)
+                for line in f.readlines():
+                    count += 1
+                    print(json.dumps(json.loads(line.strip()), indent=2))
+                print(count, 'json objects printed')
+
+    except:
+            print("your json file could not be loaded. Please make sure this script is in the same working directory as your json file.")
 
 if __name__ == '__main__':
-	main()
+        main()
